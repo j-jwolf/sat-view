2b66cea4ce1a7a255853d36e4e6a93ae2ac1855f
j-jwolf <jjwolf@noctrl.edu>
2021-07-08 13:21:54 -0600

    removing sensitive files

--- src/install.sh
+++
@@ -1,3 +0,0 @@
-test -e ~/Documents/SatelliteImage && echo file exists || mkdir ~/Documents/SatelliteImage
-test -e ~/Documents/SatelliteImage/src && echo file exists || mkdir ~/Documents/SatelliteImage/src
-sudo apt update && sudo apt install -y imagemagick
